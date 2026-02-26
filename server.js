fbf31109cc46bbe1b41544670c322c7ac7ebbab4
harsh779 <harsh779@gmail.com>
2026-02-26 16:15:14 +0530

    cors removal

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const { createClient } = require('@libsql/client');
 const path = require('path');
-const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
 app.use(express.static(path.join(__dirname, 'public'))); 
 
 const db = createClient({
